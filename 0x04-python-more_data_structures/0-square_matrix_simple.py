5668a87f06d2ef4e6438364d922406ec4d761b2b
Alausa <alausaabdulazeez@gmail.com>
2022-12-07 06:20:41 -0800

    square main

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
 
-square_matrix_simple = __import__('0-square_matrix_simple').square_matrix_simple
+def square_matrix_simple(matrix=[]):
 
+    new_matrix = []
 
+    for i in matrix:
 
-matrix = [
+        matrix = list(map(lambda x: x**2, i))
 
-       [1, 2, 3],
+        new_matrix.append(matrix)
 
-       [4, 5, 6],
-
-       [7, 8, 9]
-
-     ]
-
-
-
-new_matrix = square_matrix_simple(matrix)
-
-print(new_matrix)
-
-print(matrix)
+    return new_matrix
