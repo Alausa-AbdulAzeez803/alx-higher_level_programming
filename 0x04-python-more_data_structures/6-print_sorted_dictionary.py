57e1a56c650b27d226b057a877fa2bed969d5a2d
Alausa <alausaabdulazeez@gmail.com>
2022-12-07 06:36:09 -0800

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+
+    new_dict = sorted(a_dictionary.keys())
+
+    for i in new_dict:
+
+        print("{}: {}".format(i, a_dictionary[i]))
